2c4a5b65dc199e16ff6814a3c4bba1677b5ed9f5
Youngil Cho <terzeron@gmail.com>
2024-06-25 13:03:50 +0900

    add an example for currying

--- ch03/src/main/groovy/Currying.groovy
+++ ch03/src/main/groovy/Currying.groovy
@@ -1,3 +1,6 @@
+def product = { x, y -> x * y }
+def quadrate = product.curry(4)
+def octate = product.curry(8)
 
-class Currying {
-}
+println "4x3: ${quadrate.call(3)}"
+println "8x5: ${octate.call(5)}"
